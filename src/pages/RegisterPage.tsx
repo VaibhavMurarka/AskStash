5b87c9aee24caa3ae62754702ee1c06c67bf1b08
Vaibhav Murarka <vaibhavmurarka@192.168.1.19>
2025-07-10 13:29:18 +0530

    initialised2

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -32,7 +32,7 @@ const RegisterPage: React.FC = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Create your account
+            Create your AskStash account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
